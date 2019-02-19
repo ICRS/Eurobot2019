8a08e84be96c64c907580fee1e46ece7bdb5205a
Nick Hafner <nrh16@ic.ac.uk>
2019-02-19 13:37:59 +0000

    Created generic MessageInterface class, updated path_planning_node to use it

--- src/eurobot2019_path_planning_module/path_plan/path_planning_node.cpp
+++ src/eurobot2019_path_planning_module/path_plan/path_planning_node.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 
-#include "tactics_interface.hpp"
+#include "message_interface.hpp"
+#include <string>
+#include <sstream>
 
 int main(int argc, char **argv) {
     // Init ROS
@@ -9,21 +11,37 @@ int main(int argc, char **argv) {
     // Get node handle
     ros::NodeHandle node_handle;
 
-    // Create TacticsInterface
-    TacticsInterface tactics_interface(&node_handle, 100, 10);
+    // Create Tactics interface
+    MessageInterface<std_msgs::Empty, std_msgs::Empty> 
+                tactics_interface(100, "path_planning_result", 
+                                  10, "path_planning_cmd");
+
+    // Create Localisaion interface
+    MessageSubscriberInterface<std_msgs::Empty>
+                localisation_interface(50, "localisation_position");
+
+    // Create Path Follower interface
+    MessagePublisherInterface<std_msgs::Empty>
+                path_follower_interface(20, "path_follower_interface");
 
     // Control loop rate to be 100 Hz
     ros::Rate loop_rate(100);
+    
+    int count = 0;
 
     // Main loop
     while(ros::ok()) {
         // Do stuff
-        
-        std_msgs::Empty msg;
-        tactics_interface.publish(msg);
+        // E.g. some calculations, create messages, read messages etc.
 
-        // Check for updates
-        ros::spinOnce();
+        // Read messages from relavent interfaces
+        auto received_tactics_msg = tactics_interface.get_msg();
+        auto received_localisation_msg = localisation_interface.get_msg();
+        
+        // Create a message and set the current message t be sent
+        // This will continue to be sent until a new message is set
+        std_msgs::Empty path_msg;
+        path_follower_interface.set_msg(path_msg);
 
         // Keep update frequency
         loop_rate.sleep();
