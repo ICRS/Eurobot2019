ad3cba5ac9b4aa0c8775588b4504843d1f1cbfe3
jediknightnapoleon <1091070986@qq.com>
2019-03-06 17:01:27 +0000

    test

--- mbed/wheel_speed_control_test/wheel_controller.cpp
+++ mbed/wheel_speed_control_test/wheel_controller.cpp
@@ -1,1 +1,54 @@
 #include "wheel_controller.hpp"
+
+class WheelController{
+
+    WheelController(PinName pwm, PinName dir,
+                    PinName enc_a, PinName enc_b);
+    ~WheelController();
+
+    // Update function to be called regularly by main loop
+void WheelController::update(float dt_s) {
+
+
+
+
+
+    void set_target_vel(float vel_mm){
+      target_vel_ = vel_mm;
+    };
+
+    void get_current_vel(){
+      return current_vel_;
+    };
+
+    // PID constants
+    float kp, ki, kd;
+
+    // Returns the velocity of the wheel
+    float calculate_velocity();
+
+    // Calculate PID returns a result between -1 and 1
+    float calculate_PID(float error, float dt_s);
+
+    // Write the signed velocity (between -1 and 1) to the motor
+    void write_motor_values(float velocity);
+
+    // Pins to control the motor via h-bridge
+    PwmOut pwm_;
+    DigitalOut dir_;
+
+    // Quadrature Encoder Interface to measure the speed of the motor
+    QEI encoder_;
+
+    // Integrated error for PID
+    float int_err_;
+
+    // Previous error
+    float prev_err_;
+
+    // Target velocity
+    float target_vel_;
+
+    // Current velocity (for odometry?)
+    volatile float current_vel_;
+};
