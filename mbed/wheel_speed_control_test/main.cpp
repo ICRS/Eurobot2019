20e8e65b389f6a888f4a3e3f318147edcd8291b9
omh18 <46792269+omh18@users.noreply.github.com>
2019-03-07 22:04:34 +0100

    Updated main.cpp
    Bare bones of main added, no I2C, no sending of odometry data (i.e. current_vel_) to Pi.
    
    Question: If I2C only allows master to send to slave, how will mbed communicate speeds to Pi to allow for odometry?

--- mbed/wheel_speed_control_test/main.cpp
+++ mbed/wheel_speed_control_test/main.cpp
@@ -1,8 +1,22 @@
 #include "mbed.h"
+#include "wheel_controller.hpp"
 
 int main() {
-    while(true) {
+  // inputs for WheelController object (Pin numbers) in order:
+  // pwm, dir, enc_a, enc_b
+    WheelController controller(NC, NC, NC, NC);
 
+    // Time keeping
+    Timer timer;
+    timer.start();
+    float dt;
+
+    while(true) {
+      // Update the PID controller
+      dt = timer.read();
+      timer.reset();
+
+      controller.update(dt);
     }
 
     return 0;
