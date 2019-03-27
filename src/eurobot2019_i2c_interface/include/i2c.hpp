98bd68a204e1f0bc2481954c2b7e6d43b7431082
Nick Hafner <nrh16@ic.ac.uk>
2019-03-27 17:41:06 +0000

    Made i2c class

---
+++ src/eurobot2019_i2c_interface/include/i2c.hpp
@@ -0,0 +1,37 @@
+#ifndef __I2C_HPP__
+#define __I2C_HPP__
+
+#include <map>
+#include <int>
+
+#ifdef __PC_TEST__
+#include <iostream> // for debugging
+#else
+#include <pigpio>
+#endif // __PC_TEST__
+
+#define I2C_BUS 0
+
+#define DRIVE_ADDR 0x00
+#define GRABBER_ADDR 0x00
+#define DROPPER_ADDR 0x00
+
+enum I2CMessageType {
+    DRIVE,
+    GRABBER,
+    DROPPER
+};
+
+class I2C {
+public:
+    I2C();
+    ~I2C();
+    // Write a *message* to the *target*
+    void write(I2CMessageType target, std::string message);
+    // Read a message of length *len* to the *target*
+    std::string read(I2CMessageType target, unsigned short len);
+private:
+    std::map<I2CMessageType, int> handles_;
+};
+
+#endif // __I2C_HPP__
